41972c5b3860238644d1624b9e52aebcfcf167ae
Abere Fejiro <aberefejiro@gmail.com>
2024-02-24 15:07:19 +0100

    :file: file changes

--- src/script.js
+++ src/script.js
@@ -1,17 +1,32 @@
-let pingForm = document.getElementById('pingForm');
-// let pingButton = document.getElementById('pingButton');
-let emailInput = document.getElementById('emailInput');
-let errorMessage = document.getElementById('errorMessage');
+/*
+  * In this code, email verification and error messages
+  * are displayed depending on if a value is missing or
+  * if there is a mismatch in the pattern provided in our html
+  * when the button is clicked.
 
-pingForm.addEventListener('click', (event) => {
-    event.preventDefault();
+  * Also a modal will pop up when the email has been verified.
+  * This modal can be closed t=when the close (x) button is clicked.
+*/
 
-    if (event.target.element = 'BUTTON') {
-        console.log('click');
-        if (!emailInput.checkValidity()) {
-            errorMessage.innerHTML = "Please provide a valid email address";
-        } else {
-            errorMessage.innerHTML = "Whoops! It looks like you forgot to add your email";
-        }
-    }
-})
+const pingButton = document.getElementById("pingButton");
+const emailInput = document.getElementById("emailInput");
+const errorMessage = document.getElementById("errorMessage");
+
+pingButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  verifyEmail();
+});
+
+function verifyEmail() {
+  if (emailInput.validity.valueMissing) {
+    errorMessage.innerHTML =
+      "Whoops! It looks like you forgot to add your email";
+  } else if (emailInput.validity.patternMismatch) {
+    errorMessage.innerHTML = "Please provide a valid email address";
+  } else {
+    errorMessage.innerHTML = "";
+    showSuccessMessage();
+  }
+}
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {
       fontFamily: {
@@ -22,5 +22,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
-
+};
